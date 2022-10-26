a74d309c6002dc78b73be81a171983070649df10
AChelyadinskiy <chelyadinskiy@gmail.com>
2022-10-26 12:30:14 +0200

    circle methods implemented

--- circle.py
+++ circle.py
@@ -1,6 +1,13 @@
 from base_figure import BaseFigure
+from math import pi
 
 
 class Circle(BaseFigure):
     def __init__(self, r):
         self.r = r
+
+    def square(self):
+        return pi * self.r ** 2
+
+    def perimeter(self):
+        return 2 * pi * self.r
