fd9d81b4151ebe269ab7a7e6b13bedc35e18d073
AChelyadinskiy <chelyadinskiy@gmail.com>
2022-10-26 11:54:00 +0200

    square and perimeter methods implemented

--- rectangle.py
+++ rectangle.py
@@ -5,3 +5,9 @@ class Rectangle(BaseFigure):
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
+    def square(self):
+        return self.a * self.b
+
+    def perimeter(self):
+        return 2 * (self.a + self.b)
